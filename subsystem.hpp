1fe3431bcd90649552003f368547416e2f8c51d0
William Kunkel <will@wkunkel.com>
2013-05-22 18:56:42 -0400

    Added Subsystem class

---
+++ subsystem.hpp
@@ -0,0 +1,8 @@
+#ifndef SUBSYSTEM_HPP_INCLUDED
+#define SUBSYSTEM_HPP_INCLUDED
+
+// This class is empty because it needs no specifics but provides a common
+// interface for subsystems.
+class Subsystem {};
+
+#endif // SUBSYSTEM_HPP_INCLUDED
